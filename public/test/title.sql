98a211a34a7b35196810a2458dc413e32f8a63e1
Cameron McKenzie <cwmckenz@gmail.com>
2024-06-30 23:19:10 -0400

    titles fix

--- public/test/title.sql
+++ public/test/title.sql
@@ -1,4 +1,5 @@
-insert or replace into titles ( title_id, title_name, bgg, is_symmetric ) values ( 'arcs', 'Arcs', 359871, 1 );
+delete from titles;
+insert or replace into titles ( title_id, title_name, bgg, is_symmetric ) values ( 'test', 'Arcs', 359871, 1 );
 insert or ignore into setups ( title_id, player_count, scenario, options ) values
 	( 'arcs', 2, '2P', '{"open_discard":true,"leaders_lore":true,"leaders_pack":true}' ),
 	( 'arcs', 3, '3P', '{"open_discard":true,"leaders_lore":true,"leaders_pack":true}' ),
